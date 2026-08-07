64a02082356c49d377dea67a527abd2e84671631
ttcoolman <trmcdona@usc.edu>
2026-08-06 22:16:55 -0400

    UVM-alu and AXI folder

--- Digital IC Degin Class Projects/personal projects/UVM-alu/alu_driver.sv
+++ Digital IC Degin Class Projects/personal projects/UVM-alu/alu_driver.sv
@@ -8,11 +8,15 @@ class alu_driver extends uvm_driver (alu transaction);
 
 
     virtual alu_if vif;
+//Creates a constructor in system verilog
+//Whenever anyone creates an alu driver object it calls this function 
+//The default name for an object will be alu_driver
+function new (string name = "alu_driver", uvm_component parent = null);
+super.new(name , parent);
+endfunction 
 
+functio
 
-vif.A <= transaction.A;
-vif.B <= transaction.B;
-vif.ALUControl <= transaction.ALUControl;
 
 
 
